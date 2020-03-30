36570f7b03b3b7ada31560689d80966695d6d2d9
Ibrahim Rahmani <ibrahim@xala.no>
2020-03-30 19:34:12 +0200

    Back to SQLlite

--- rta/settings.py
+++ rta/settings.py
@@ -83,18 +83,11 @@ WSGI_APPLICATION = 'rta.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'OPTIONS': {
-             'sql_mode': 'traditional',
-         },
-         'NAME': 'rtaaff2_mediahub',
-         'USER': 'rtaaff2_mediahub',
-         'PASSWORD': 'rta@2020',
-         'HOST': 'localhost',
-         'PORT': '3306',
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
